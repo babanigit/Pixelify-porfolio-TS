cdee7ae188bad5ebaba3b720bcb183869deb50d6
bablu <aniketpanchal07@gmail.com>
2024-02-16 18:14:41 +0530

    new email component added

--- src/conteners/4contactMe/ContactMe.tsx
+++ src/conteners/4contactMe/ContactMe.tsx
@@ -5,13 +5,12 @@ interface Iprops {
   theme: ThemeDataType;
 }
 const ContactMe = ({ theme }: Iprops) => {
-  const resu = "<Resume/>"
+  const resu = "<Resume/>";
   return (
-    <div className=" bg-red-200 h-screen w-screen grid grid-flow-col grid-cols-3 place-items-center items-center">
-      
+    <div className="  h-screen w-screen grid grid-flow-col grid-cols-3 place-items-center items-center">
       {/* div 1  */}
 
-      <div className=" bg-green-200 h-full w-full grid justify-center items-center">
+      <div className="  h-full w-full grid justify-center items-center">
         <img
           className="profile-pic w-64"
           src={require(`../../assets/images/me306kb.jpg`)}
@@ -19,26 +18,34 @@ const ContactMe = ({ theme }: Iprops) => {
         />
       </div>
 
-{/* div 2 */}
+      {/* div 2 */}
 
-      <div className=" h-[60%] w-[60%] col-span-2 grid-rows-4 bg-blue-200 grid justify-center items-center p-[3%] py-[10%] ">
-        <div className=" text-3xl grid justify-center ">Contact Me</div>
-        
-        <div className=" grid row-span-2 justify-center items-center " >
-          <form action="" className=" grid gap-2 grid-flow-row  ">
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
-            <button className=" justify-end grid"> Send</button>          </form>
+      <div className=" bg-gray-400 h-[60%] w-[60%] col-span-2 grid-rows-4  grid items-center p-[3%] ">
+
+        <div className=" h-full  text-3xl grid justify-center items-center  row-span-1 ">
+          Contact Me
         </div>
-        
-        <div className=" grid justify-end ">
+
+        <div className=" h-full grid row-span-2  ">
+          <form action="" className=" grid gap-2 grid-flow-row place-items-end ">
+            <input type="text" style={{background:theme.body}} placeholder="your email" className="border border-5 border-white w-full " />
+            <input type="text" style={{background:theme.body}} placeholder="message" className=" h-[100px] border border-5 border-white w-full " />
+            <button 
+            style={{borderColor:theme.body}}
+            className=" w-[20%]  grid border border-5 "> Send</button>{" "}
+          </form>
+        </div>
+
+        <div className=" h-full grid justify-end items-center  ">
           {" "}
-          <a className=" border-4 p-[8%] w-[120px]  " href="./Resume.pdf">
+          <a 
+                      style={{borderColor:theme.body}}
+
+          className=" border-4 p-[8%] w-[120px]  " href="./Resume.pdf">
             <a
-              href={
-                "https://drive.google.com/file/d/1vrH2TG_69x0BslVAYGnNDpzCPCczuPXb/view?usp=sharing/view"
-              }
+              // href={
+              //   "https://drive.google.com/file/d/1vrH2TG_69x0BslVAYGnNDpzCPCczuPXb/view?usp=sharing/view"
+              // }
               target="_blank"
               rel="noreferrer"
             >
@@ -46,6 +53,7 @@ const ContactMe = ({ theme }: Iprops) => {
             </a>
           </a>
         </div>
+
       </div>
     </div>
   );
